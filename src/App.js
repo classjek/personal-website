5f36cba8ac6132e7627d8165db0d8b02c69c2ce6
classjek <jekoniak13@gmail.com>
2023-12-25 18:46:04 -0800

    unstyled navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+// Import page components
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+
+// Define Navbar
+function Navbar() {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      hello world
+    <div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </Routes>
+    </Router>
+    <div>Say a thing</div>
     </div>
   );
 }
